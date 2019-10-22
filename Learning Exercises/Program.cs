528353079da7c3cfeb85578f12148d20c68e400e
P N <p.newbranch@outlook.com>
2019-10-22 10:01:56 +0200

    Add project files.

---
+++ Learning Exercises/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Learning_Exercises
+{
+    class Program
+    {
+        private static void RunExerciseOne()
+        {
+            Console.WriteLine("You successfully ran exercise one!");
+        }
+        
+        private static void RunExerciseTwo()
+        {
+            Console.WriteLine("You successfully ran exercise two!");
+        }
+
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+
+            var keepAlive = true;
+            while (keepAlive)
+            {
+                try
+                {
+                    Console.Write("Enter assignment number (or -1 to exit): ");
+                    var assignmentChoice = int.Parse(Console.ReadLine() ?? "");
+                    switch (assignmentChoice)
+                    {
+                        case 1:
+                            RunExerciseOne();
+                            break;
+                        case 2:
+                            RunExerciseTwo();
+                            break;
+                        case 3:
+                            // Call your next assignment method here!
+                            break;
+                        case -1:
+                            keepAlive = false;
+                            break;
+                        default:
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("That is not a valid assignment number!");
+                            break;
+                    }
+                    Console.ResetColor();
+                    Console.WriteLine("Hit any key to continue");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+                catch
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Tha is not a valid assignment number!");
+                    Console.ResetColor();
+                }
+            }
+        }
+    }
+}
